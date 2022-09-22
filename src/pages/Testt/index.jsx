63944440c28316af95f66cbd6cd2097be4df58b1
ThienLe <thanhthienctvq@gmail.com>
2022-09-23 00:01:55 +0700

    content carousel + about

--- src/pages/Testt/index.jsx
+++ src/pages/Testt/index.jsx
@@ -1,108 +1,89 @@
-// import React from 'react';
-// import "./T.scss";
+import { Container } from '@mui/system';
+import React from 'react';
+import ProductItem from '../../components/ProductItem';
+import "./Testt.scss";
 
-// function Testt(props) {
-//     return (
-//         <div className="bbb_viewed">
-//         <div className="container">
-//             <div className="row">
-//                 {/* <div className="col"> */}
-//                    <div className="bbb_main_container">
-//                     <div className="bbb_viewed_title_container">
-//                         <h3 className="bbb_viewed_title">Best selling products</h3>
-//                         <div className="bbb_viewed_nav_container">
-//                             <div className="bbb_viewed_nav bbb_viewed_prev"><i className="fas fa-chevron-left"></i></div>
-//                             <div className="bbb_viewed_nav bbb_viewed_next"><i className="fas fa-chevron-right"></i></div>
-//                         </div>
-//                     </div>
-//                     <div className="bbb_viewed_slider_container">
-//                         <div className="owl-carousel owl-theme bbb_viewed_slider">
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924153/alcatel-smartphones-einsteiger-mittelklasse-neu-3m.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹12225<span>₹13300</span></div>
-//                                         <div className="bbb_viewed_name"><a href="#">Alkatel Phone</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924221/51_be7qfhil.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹30079</div>
-//                                         <div className="bbb_viewed_name"><a href="#">Samsung LED</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924241/8fbb415a2ab2a4de55bb0c8da73c4172--ps.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹22250</div>
-//                                         <div className="bbb_viewed_name"><a href="#">Samsung Mobile</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item is_new d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924275/images.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹1379</div>
-//                                         <div className="bbb_viewed_name"><a href="#">Huawei Power</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924361/21HmjI5eVcL.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹225<span>₹300</span></div>
-//                                         <div className="bbb_viewed_name"><a href="#">Sony Power</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                             <div className="owl-item">
-//                                 <div className="bbb_viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-//                                     <div className="bbb_viewed_image"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924241/8fbb415a2ab2a4de55bb0c8da73c4172--ps.jpg" alt="" /></div>
-//                                     <div className="bbb_viewed_content text-center">
-//                                         <div className="bbb_viewed_price">₹13275</div>
-//                                         <div className="bbb_viewed_name"><a href="#">Speedlink Mobile</a></div>
-//                                     </div>
-//                                     <ul className="item_marks">
-//                                         <li className="item_mark item_discount">-25%</li>
-//                                         <li className="item_mark item_new">new</li>
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                    </div> 
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//     );
-// }
+function Testt(props) {
+    return (
+        <div  className="container-fluid profuid">
+    <Container className="title-list">
+    <h2>KHUYẾN MÃI HOT</h2>
+        <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+               
+                <div id="myCarousel" className="carousel slide" data-ride="carousel" data-interval="0">
+                {/* <!-- Carousel indicators --> */}
+                {/* <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>    */}
+                {/* <!-- Wrapper for carousel items --> */}
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="row">
+                            <div className="col-sm-3">
+                               <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>		
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>								
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="row">
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>						
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className="row">
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>
+                            <div className="col-sm-3">
+                            <ProductItem />
+                            </div>						
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- Carousel controls --> */}
+                <a className="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                    <i className="fa fa-angle-left"></i>
+                </a>
+                <a className="carousel-control-next" href="#myCarousel" data-slide="next">
+                    <i className="fa fa-angle-right"></i>
+                </a>
+            </div>
+            </div>
+        </div>
+    </div>
+    </Container>
+    </div>
+    );
+}
 
-// export default Testt;
+export default Testt;

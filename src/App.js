22ee185b963cb1f4cbf8b0789507d856c4a510ec
Lê Thanh Thiên <107629259+Thienlu99@users.noreply.github.com>
2022-10-05 08:35:49 +0700

    update folder content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Breadcrumb from './components/Breadcrumb';
-import Imgbackground from './components/bg';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/customer/Home';
+import Register from './pages/customer/Register';
+import Login from './pages/customer/Login';
+import Question from './pages/customer/Question';
+import Security from './pages/customer/AboutSecurity';
+import Header from './layouts/customer/Header';
+import Nav from './components/customer/Nav';
+import Footer from './layouts/customer/Footer';
+import ProducitemListSearch from './pages/customer/SearchItem/ProducitemListSearch';
+import PhoneItem from './pages/customer/PhoneItem';
+import CarouselPhone from './pages/customer/Carousel-PhoneItem';
+import NotFound from './pages/customer/NotFound';
+import DetalPhone from './pages/customer/PhoneItem/DetalPhone';
+import PricePhone from './pages/customer/PhoneItem/PricePhone';
+import ThemeProvider from './components/customer/Context/ThemeProvider';
+import CheckDefault from './pages/customer/CheckOder/CheckDefault';
+import Cart from './pages/customer/Cart';
+// import Testt from './features/Account/pages/Register';
+
 function App() {
   return (
   <>
+  <ThemeProvider >
+  <BrowserRouter>
     <Header />
     <Nav />
-    <Breadcrumb />
-    <Imgbackground />
-    {/* <Banner /> */}
-    <ProductList />
-    <ProductList />
-     <ProductList />
-     <Footer />
+     <main className="main-content">
+     <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/question' element={<Question />} />
+      <Route path='/aboutSecurity' element={<Security />} />
+      <Route path='/search' element={<ProducitemListSearch />} />
+      <Route  exact path='/phone' element={<PhoneItem />} />
+      {/* path={`${url}/:id`}> */}
+      <Route path={`/phones/:producer`} element={<DetalPhone />} />
+      {/* <Route path={`/phone/:producer`} element={<DetalPhone />} /> */}
+      {/* <Route path='/phone/:descriptionPrice' element={<PricePhone />} /> */}
+      {/* <Route path={`/phones/b`} element={<CarouselPhone />} /> */}
+      <Route path={`/phone/:id`}  element={<CarouselPhone />} />
+      <Route path="/checkorder"  element={<CheckDefault />} />
+      <Route path="/cart"  element={<Cart />} />
+      <Route path='*' element={<NotFound />} />
+      
+    </Routes>
+    </main>
+    <Footer />
+    {/* <Testt /> */}
+     </BrowserRouter>
+     </ThemeProvider>
     </>
   );
 }
